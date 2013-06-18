cbdc9170075f8d2638ea69b32b14b1d61408f522
kardeiz <kardeiz@gmail.com>
2013-06-18 14:39:39 -0500

    Update config_initialize.rb

--- lib/generators/virtual_shelf/initialize/templates/config_initialize.rb
+++ lib/generators/virtual_shelf/initialize/templates/config_initialize.rb
@@ -1,14 +1,14 @@
 VirtualShelf.config do |config|
-  config.catalog_link = 'http://libweb.lib.tcu.edu/F/?func=direct&doc_library=TCU01&doc_number='
-  config.copyright_statement = '&copy; TCU Library 2013'
+  config.catalog_link = 'http://catalog.tld/F/?func=direct&doc_library=LIB01&doc_number='
+  config.copyright_statement = '&copy; Your Library 2013'
   config.cache_timeout = 30.minutes
   config.thumbnails_base_url = lambda {|cid_tag|
     "http://servername.tld/covers_#{cid_tag[0..3]}_#{cid_tag[4..5]}/#{cid_tag}-M.jpg" 
   }
-  config.z11_table_name = 'TCU01.Z11'
-  config.z13_table_name = 'TCU01.Z13'
-  config.z13u_table_name = 'TCU01.Z13U'
-  config.z30_table_name = 'TCU50.Z30'
+  config.z11_table_name = 'LIB01.Z11'
+  config.z13_table_name = 'LIB01.Z13'
+  config.z13u_table_name = 'LIB01.Z13U'
+  config.z30_table_name = 'LIB50.Z30'
   config.z13u_isbn_field = 'z13u_user_defined_1'
   config.z13u_oclc_field = 'z13u_user_defined_3'
   config.z13u_contents_field = 'z13u_user_defined_4'
